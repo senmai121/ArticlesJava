ccf922d0bc8d2ce1315fd1940f023786eeca0600
senmai121 <38975764+senmai121@users.noreply.github.com>
2025-04-18 16:54:12 +0700

    Delete Library/Mobile Documents/com~apple~CloudDocs/Documents/article 2 directory

--- Library/Mobile Documents/com~apple~CloudDocs/Documents/article 2/src/main/java/article/article/JobExperienceRepository.java
+++
@@ -1,6 +0,0 @@
-package article.article;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JobExperienceRepository extends MongoRepository<JobExperience, String> {
-    // custom queries ถ้าต้องการเพิ่ม
-}
