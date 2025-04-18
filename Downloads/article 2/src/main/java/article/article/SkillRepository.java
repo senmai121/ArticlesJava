13f085b4bb93f7312ed13cab85a14d202c173629
senmai121 <38975764+senmai121@users.noreply.github.com>
2025-04-18 16:53:55 +0700

    Delete Downloads/article 2 directory

--- Downloads/article 2/src/main/java/article/article/SkillRepository.java
+++
@@ -1,6 +0,0 @@
-package article.article;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SkillRepository extends MongoRepository<Skill, String> {
-    // custom queries ถ้าต้องการเพิ่ม
-}
